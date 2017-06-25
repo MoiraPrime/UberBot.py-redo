dea1ec9ff6574ba7443135b19d4bfbb3f6fb317d
Tyler Clements <UberActivist@gmail.com>
2017-06-25 15:44:02 -0500

    Added some things....
    Updated gitignore to ignore __pycache__. Added an example plugin. Moved
    test.py to main.py.

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
-import discord
-import asyncio
-import bot_plugin
+from discord.ext import commands
+import json
 
-bot = discord.Client()
+description = '''WIP'''
+global_config = {}
 
+# this specifies what extensions to load when the bot starts up
+startup_extensions = ["members", "rng"]
+
+bot = commands.Bot(command_prefix='&', description=description)
+
+with open("config.json") as config:
+    global_config = json.load(config)
 
 @bot.event
 async def on_ready():
-    print("Logged in.")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def load(extension_name : str):
+    """Loads an extension."""
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
+
+@bot.command()
+async def unload(extension_name : str):
+    """Unloads an extension."""
+    bot.unload_extension(extension_name)
+    await bot.say("{} unloaded.".format(extension_name))
+
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+@bot.command()
+async def repeat(times : int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await bot.say(content)
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+    bot.run(global_config["token"])
