db60be43e11cc51f71da3ba220d4298977c24e6c
Tyler Clements <UberActivist@gmail.com>
2017-06-29 00:20:50 -0500

    Added raffle command.

--- plugins/misc.py
+++ plugins/misc.py
@@ -9,6 +9,7 @@ class misc():
 
     @commands.command()
     async def csi(self):
+        """Generates a random 'techy' sounding jargon sentence based on the poor writing found on CSI: Cyber"""
         start = ['The first thing we need to do is ','To fix your problem, we have to ','The hackers are getting in! Quickly, ','To get in to the government database we\'re going to ','Quickly! We have to ','We can get rid of the virus, don\'t worry. First we have to ']
         verb = ['reroute','splice','compile','reprocess','log','port','decrypt','encrypt','recode','refactor','import','export','modify','uninstall','install','upload','download','open','decode','push','recompile','decompile','write a GUI to track','trace','troubleshoot']
         noun = [' the VGA cable',' the USB',' the browser',' the interface',' the Internet',' the IP address',' the source code',' the hard drive',' the RAM',' the CPU',' the motherboard',' the monitor',' the shortcut',' the LAN',' the Wi-Fi',' the CAT5',' the Bluetooth',' the program',' the encryption',' the compiler',' the IDE',' Linux',' Microsoft Word',' the Google',' the traceroute',' the stack',' C++',' Java',' JavaScript',' C',' C#',' Python',' the programming language',' the SATA cable',' the subnet mask',' the Ethernet',' the Ethernet adapter',' the GPU',' the keyboard',' Internet Explorer',' Ubuntu',' the command prompt',' the command line',' HTTPS',' FTP',' SSH',' Visual Basic']
@@ -21,6 +22,13 @@ class misc():
             template = "{}{}{}{}{}."
             return template.format(pickOne(start), pickOne(verb), pickOne(noun), pickOne(preposition), pickOne(noun))
         await self.bot.say(doList())
+
+    @commands.command(pass_context=True)
+    async def raffle(self, ctx):
+        members = ctx.message.server.get_all_members()
+        choice = random.choice(members)
+        bot.say("I choose <@{}>".format(choice.id))
+
 
 
 def setup(bot):
