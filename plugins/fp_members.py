a7a0688a30a14c77b908fec86b52aafd1d77198e
Tyler Clements <UberActivist@gmail.com>
2017-06-28 21:49:51 -0500

    Mostly working basic functionality....
    Added fp_members, added empty file for starting a permissions module.

---
+++ plugins/fp_members.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+class fpmembers():
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_member_join(self, member):
+        if member.server != "149707514521321473":
+            return
+        template = "Everyone welcome <@{}> to the FirePowered Discord server! This brings our total number of members to {}."
+        template = template.format(member.id, member.server.member_count)
+        await self.bot.say(template)
+
+    @commands.command(pass_context=True)
+    async def members(self, ctx):
+        if ctx.message.channel.is_private:
+            return
+        template = "There are {} members in this server.".format(ctx.message.author.server.member_count)
+        await self.bot.say(template)
+
+    @commands.command(pass_context=True)
+    async def joinedat(self, ctx, member: discord.Member = None):
+        if ctx.message.channel.is_private:
+            return
+        if member is None:
+            member = ctx.message.author
+        template = "{0} joined at {0.joined_at}".format(member)
+        await self.bot.say(template)
+
+
+
+
+def setup(bot):
+    bot.add_cog(fpmembers(bot))
