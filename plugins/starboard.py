138dca097a45ac7e19d934d4c078c5600b34b871
Tyler Clements <UberActivist@gmail.com>
2018-01-28 23:25:32 -0600

    test

--- plugins/starboard.py
+++ plugins/starboard.py
@@ -4,6 +4,39 @@ import asyncio
 
 restricted = ["246758392411062272", "284903283770785792", "406279148932431872", "397963636464943105", "405135937912438804"]
 
+async def starcheck(self, message, channel):
+    reaction = None
+    message = self.bot.get_channel(channel, message)
+    for i in message.reactions:
+        if i.emoji == "⭐":
+            reaction = i
+
+    if reaction.message.server.id != "149707514521321473":
+        return
+    if reaction.message.channel.id in restricted:
+        return
+    for i in reaction.message.reactions:
+        if i.emoji == "❌" and i.me:
+            return
+        if i.emoji == "✅" and i.me:
+            return
+    if reaction.message.author.id == member.id:
+        await self.bot.add_reaction(reaction.message, "❌")
+        return
+
+    if reaction.count < 3:
+        return
+
+    embed=discord.Embed(color=0xff8000, description=reaction.message.content)
+    embed.set_author(name=reaction.message.author.name, icon_url=reaction.message.author.avatar_url)
+    if len(reaction.message.attachments) > 0:
+        embed.set_image(url=reaction.message.attachments[0]["url"])
+    date = reaction.message.timestamp
+    time = reaction.message.timestamp
+    embed.set_footer(text="{}/{}/{} at {}:{} in #{}.".format(date.month, date.day, date.year, time.hour, time.minute, reaction.message.channel.name))
+    await self.bot.send_message(self.bot.get_channel('405135937912438804'), "New Star!", embed=embed)
+    await self.bot.add_reaction(reaction.message, "✅")
+
 class starboard():
     """Firepowered Server starboard plugin."""
     def __init__(self, bot):
@@ -37,9 +70,10 @@ class starboard():
         await self.bot.add_reaction(reaction.message, "✅")
 
     @commands.command()
-    async def choose2(self, *choices: str):
-        """Chooses between multiple choices."""
-        await self.bot.say(random.choice(choices))
+    async def starcheck(self, message: str, channel: discord.Channel):
+        """Check a message for stars."""
+        await self.bot.say("Alright, let me run a star check.")
+        await starcheck(self, message, channel)
 
 
 def setup(bot):
